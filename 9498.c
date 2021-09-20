8e68faf3a1f298b51e915aad1849baba7f674c1a
siyeon lee <temperlooser@gmail.com>
2021-09-20 17:53:36 +0900

    Add files via upload

---
+++ 9498.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+	int score;
+		scanf("%d",&score);
+	char grade;
+	switch (score/10) {
+		case 10 :
+		case 9 :
+			grade='A';
+			break;
+		case 8 :
+			grade='B';
+			break;
+		case 7 :
+			grade='C';
+			break;
+		case 6 :
+			grade='D';
+			break;
+		default :
+			grade='F';
+			break;
+	}
+	printf("%c",grade);
+
+	}
+		
