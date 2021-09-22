14ca73715bc39dcfe2582894f2dda5b54c91c9dd
siyeon lee <temperlooser@gmail.com>
2021-09-22 17:53:16 +0900

    Add files via upload

--- 2587.py
+++ 2587.py
@@ -1,7 +1,30 @@
 nums: int = []
-for i in range(0,5):
+for i in range(0,9):
     nums.append(int(input()))
 
-print(int(sum(nums)/5))
-print(sorted(nums)[2])
+sums = sum(nums)
+cha = sums - 100
+
+i = 0
+j = 8
+
+nums.sort()
+while j>i:
+    temp = nums[i] + nums[j]
+    if(temp > cha):
+        j -= 1
+    elif(temp < cha):
+        i += 1
+    else:
+        break
+
+a= nums[i]
+b= nums[j]
+
+
+nums.remove(a)
+nums.remove(b)
+
+for i in nums:
+    print(i, end = ' ')
 
